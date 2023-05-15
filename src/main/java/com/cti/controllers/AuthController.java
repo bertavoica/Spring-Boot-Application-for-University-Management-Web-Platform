7ce97ce5fdfbf90b197a6d934807b1408cc2d5f0
bertavoica <bertavoica@gmail.com>
2023-05-15 18:14:54 +0300

    Refactored auth controller

--- src/main/java/com/cti/controllers/AuthController.java
+++ src/main/java/com/cti/controllers/AuthController.java
@@ -1,55 +1,21 @@
 package com.cti.controllers;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import javax.validation.Valid;
 
 import com.cti.exception.EmailAlreadyInUseException;
 import com.cti.exception.UsernameAlreadyTakenException;
-import com.cti.models.*;
 import com.cti.payload.request.LoginRequest;
 import com.cti.payload.request.SignupRequest;
-import com.cti.payload.response.JwtResponse;
 import com.cti.payload.response.MessageResponse;
-import com.cti.repository.RoleRepository;
-import com.cti.repository.StudentRepository;
-import com.cti.repository.UserRepository;
-import com.cti.security.jwt.JwtUtils;
-import com.cti.security.services.UserDetailsImpl;
 import com.cti.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/auth-controller")
 public class AuthController {
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
-
-    @Autowired
-    private PasswordEncoder encoder;
-
-    @Autowired
-    private JwtUtils jwtUtils;
-
-    @Autowired
-    private StudentRepository studentRepository;
 
     @Autowired
     private AuthService authService;
