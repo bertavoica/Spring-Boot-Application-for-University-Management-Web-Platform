4291d66215e3a11f15fd129f534f1d6c497c6d59
bertavoica <bertavoica@gmail.com>
2023-05-29 12:05:16 +0300

    Added some tests

--- src/test/java/com/cti/service/UserServiceTests.java
+++ src/test/java/com/cti/service/UserServiceTests.java
@@ -1,5 +1,7 @@
 package com.cti.service;
 
+import com.cti.exception.UserNoLanguageException;
+import com.cti.exception.UserNotFoundException;
 import com.cti.models.Title;
 import com.cti.models.User;
 import com.cti.repository.StudentRepository;
@@ -16,8 +18,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -46,5 +49,46 @@ public class UserServiceTests {
         List<User> result = this.userService.getAllUsers();
 
         assertEquals(2, result.size());
+    }
+
+
+    @Test
+    @DisplayName("Get language preference successfully.")
+    public void getLanguagePreferenceTest() throws UserNotFoundException, UserNoLanguageException {
+        User user = new User();
+        user.setUsername("test");
+        user.setLanguagePreference("romanian");
+
+        Mockito.when(this.userRepository.findByUsername("test")).thenReturn(Optional.of(user));
+
+        String result = this.userService.getLanguagePreference("test");
+
+        assertEquals("romanian", result);
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("Get language preference when user not exists.")
+    public void getLanguagePreferenceWhenUserNotExistsTest() {
+        User user = new User();
+        user.setUsername("test_1");
+        user.setLanguagePreference("romanian");
+
+        Mockito.when(this.userRepository.findByUsername("test")).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> this.userService.getLanguagePreference("test"));
+    }
+
+    @Test
+    @DisplayName("Get language preference when user has no language successfully.")
+    public void getLanguagePreferenceWhenUserHasNoLanguageTest() {
+        User user = new User();
+        user.setUsername("test");
+        user.setLanguagePreference(null);
+
+        Mockito.when(this.userRepository.findByUsername("test")).thenReturn(Optional.of(user));
+
+        assertThrows(UserNoLanguageException.class, () -> this.userService.getLanguagePreference("test"));
+
     }
 }
