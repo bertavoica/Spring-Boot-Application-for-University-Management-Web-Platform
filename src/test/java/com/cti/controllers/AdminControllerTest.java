44b90cfaa7e7d2d8304958975b7860ea9e253ba6
bertavoica <bertavoica@gmail.com>
2023-05-29 18:27:09 +0300

    Added some tests

--- src/test/java/com/cti/controllers/AdminControllerTest.java
+++ src/test/java/com/cti/controllers/AdminControllerTest.java
@@ -1,66 +1,85 @@
 package com.cti.controllers;
 
-import com.cti.controllers.AdminController;
 import com.cti.models.Admin;
-import com.cti.models.ERole;
-import com.cti.models.Role;
-import com.cti.models.Student;
-import com.cti.models.Teacher;
 import com.cti.models.User;
 import com.cti.payload.request.AdminUpdateRequest;
-import com.cti.repository.RoleRepository;
-import com.cti.repository.StudentRepository;
-import com.cti.repository.TeacherRepository;
-import com.cti.repository.UserRepository;
-import com.cti.service.UserService;
+import com.cti.service.AdminService;
+import com.google.gson.Gson;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
-import static org.testng.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AdminControllerTest {
 
-    @Mock
-    private TeacherRepository teacherRepository;
+    private MockMvc mockMvc;
 
     @Mock
-    private UserRepository userRepository;
-
-    @Mock
-    private RoleRepository roleRepository;
-
-    @Mock
-    private StudentRepository studentRepository;
-
-    @Mock
-    private UserService userService;
-
-    @Mock
-    private Principal principal;
+    private AdminService adminService;
 
     @InjectMocks
     private AdminController adminController;
 
+    private static final String URL = "/admin-controller";
+
     @BeforeMethod
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(adminController).build();
     }
 
     @Test
-    public void getAllAdmins_WithUsername_ShouldReturnResponseEntityWithListOfAdmins() {
-        // Arrange
+    public void getAllAdminsSuccessfully() throws Exception {
+        String username = "username";
+
+        Mockito.when(this.adminService.prepareAdminList(username)).thenReturn(List.of(new Admin(new User())));
+
+        this.mockMvc.perform(get(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("username", username))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteAdminSuccessfully() throws Exception {
+        String username = "admin_username";
+
+        Mockito.doNothing().when(this.adminService).deleteByUsername(username);
+
+        mockMvc.perform(delete(URL)
+                        .param("username", username)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    public void updateAdminSuccessfully() throws Exception {
+        AdminUpdateRequest adminUpdateRequest = new AdminUpdateRequest();
+        adminUpdateRequest.setRole("role");
+        adminUpdateRequest.setUsername("username");
+
+        Mockito.when(this.adminService.updateAdmin(eq(adminUpdateRequest), Mockito.any(Principal.class))).thenReturn(List.of(new Admin(new User())));
+
+        mockMvc.perform(put(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new Gson().toJson(adminUpdateRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
